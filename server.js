7aa0b83e5d3716f291ad03b3f8aa892e4bad9021
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-03 00:46:45 +0700

    :hotsprings: [Sequealize][postgresql]: Created Establishing check for the connection

--- server.js
+++ server.js
@@ -2,8 +2,21 @@ const express = require('express');
 const dotenv = require('dotenv');
 const app = express();
 
+// importing database
+const models = require('./models');
+
 // Load env vars
 dotenv.config({ path: './config/config.env' });
+
+// Establish the Sequelize connection with the database
+models.sequelize
+	.authenticate()
+	.then(function() {
+		console.log('Connection successful');
+	})
+	.catch(function(error) {
+		console.log('Error creating connection:', error);
+	});
 
 const PORT = process.env.PORT || 5000;
 
