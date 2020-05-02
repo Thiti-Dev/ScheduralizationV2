f4572e08657c5f079b3611afffcffaa70515e011
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-03 01:57:25 +0700

    :m: [middleware][util]: Created errorHandler , ErrorResponse

---
+++ middleware/error.js
@@ -0,0 +1,33 @@
+const ErrorResponse = require('../utils/errorResponse');
+
+const errorHandler = (err, req, res, next) => {
+	let error = { ...err };
+
+	error.message = err.message;
+
+	//Log to console for dev
+	console.log(err);
+
+	// Unexpected token (json) handler
+	// Handler for custom raw json self-inpur
+	if (err.type === 'entity.parse.failed') {
+		const message = 'Invalid data ( Unexpected token )';
+		error = new ErrorResponse(message, 400);
+	}
+
+	//
+	// ─── VALIDATION ERROR ───────────────────────────────────────────────────────────
+	//
+	if (error.validateErrors) {
+		//error.statusCode = 400;
+		error.message = error.validateErrors;
+	}
+	// ────────────────────────────────────────────────────────────────────────────────
+
+	res.status(error.statusCode || 500).json({
+		success: false,
+		errors: error.message || 'Server Error'
+	});
+};
+
+module.exports = errorHandler;
