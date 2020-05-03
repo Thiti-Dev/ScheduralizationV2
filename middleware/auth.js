7cd8cc74cdf158576639803440f15cd8bbf8f426
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-03 22:44:58 +0700

    :closed_lock_with_key::m: [auth-middleware]: Created protect

---
+++ middleware/auth.js
@@ -0,0 +1,33 @@
+const jwt = require('jsonwebtoken');
+const asyncHandler = require('./async');
+const ErrorResponse = require('../utils/errorResponse');
+const { User } = require('../models');
+
+// Protect routes
+exports.protect = asyncHandler(async (req, res, next) => {
+	let token;
+	if (req.cookies.token) {
+		token = req.cookies.token;
+	}
+
+	// Make sure token exist
+	if (!token) {
+		return next(new ErrorResponse('Not authorize to access this route', 401));
+	}
+
+	try {
+		// Verify token
+		const decoded = jwt.verify(token, process.env.JWT_SECRET);
+
+		//console.log(decoded);
+
+		req.user = await User.findByPk(decoded.id);
+
+		//console.log(req.user);
+
+		next();
+	} catch (err) {
+		console.log(err);
+		return next(new ErrorResponse('Not authorize to access this route', 401));
+	}
+});
