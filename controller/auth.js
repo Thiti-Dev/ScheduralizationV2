fcc5782109b331ad8bbe7d26bae26647a894d7ab
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-03 21:35:08 +0700

    :zap: [controller]: Created login

--- controller/auth.js
+++ controller/auth.js
@@ -2,6 +2,15 @@ const asyncHandler = require('../middleware/async');
 const ErrorResponse = require('../utils/errorResponse');
 const { User } = require('../models');
 
+//
+// ─── UTIL ───────────────────────────────────────────────────────────────────────
+//
+const sendTokenResponse = require('../utils/tokenResponse');
+// ────────────────────────────────────────────────────────────────────────────────
+
+// @desc    Register user
+// @route   POST /api/auth/
+// @acess   Public
 exports.register = asyncHandler(async (req, res, next) => {
 	const user = await User.create({
 		email: req.body.email,
@@ -11,4 +20,25 @@ exports.register = asyncHandler(async (req, res, next) => {
 		studentID: req.body.studentID
 	});
 	res.status(200).json({ success: true, data: user });
+});
+
+// @desc    Login user
+// @route   POST /api/auth/login
+// @acess   Public
+exports.login = asyncHandler(async (req, res, next) => {
+	const user = await User.findOne({
+		where: {
+			email: req.body.email
+		}
+	});
+	if (!user) {
+		return next(new ErrorResponse(`Invalid credentials`, 400));
+	}
+	if (!await user.matchPassword(req.body.password)) {
+		return next(new ErrorResponse(`Invalid credentials`, 400));
+	}
+	// Exclude the field after checked from above
+	user.password = undefined;
+	//res.status(200).json({ success: true, data: user });
+	sendTokenResponse(user, 200, res);
 });
--- routes/api/auth.js
+++ routes/api/auth.js
@@ -5,7 +5,7 @@ const { register, login } = require('../../controller/auth');
 
 // @desc Importing validator
 const validateRegister = require('../../utils/validations/register');
-
+const validateLogin = require('../../utils/validations/login');
 router.route('/').post(validateRegister, register);
-
+router.route('/login').post(validateLogin, login);
 module.exports = router;
