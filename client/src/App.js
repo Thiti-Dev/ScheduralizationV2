a1ac13ea098362b8e42805be538dad8afd4c399e
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-15 21:51:59 +0700

    :crystal_ball: [mobx][init]: Created combinedStore , passed into provider

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,37 @@
 import React, { Component } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Landing from './components/pages/Landing';
+import Home from './components/pages/Home';
 import './App.css';
+
+//
+// ─── MOBX ───────────────────────────────────────────────────────────────────────
+//
+import { Provider } from 'mobx-react';
+
+import { RootStore } from './mobx/stores/RootStore';
+const _RootStore = new RootStore();
+// ────────────────────────────────────────────────────────────────────────────────
+
+//
+// ─── COMBINE MOBX STORE ─────────────────────────────────────────────────────────
+//
+const store = {
+	rootStore: _RootStore,
+	globalStore: _RootStore.globalStore
+};
+// ────────────────────────────────────────────────────────────────────────────────
 export default class App extends Component {
 	render() {
 		return (
-			<Router>
-				<Switch>
-					<Route exact path="/" component={Landing} />
-				</Switch>
-			</Router>
+			<Provider {...store}>
+				<Router>
+					<Switch>
+						<Route exact path="/" component={Landing} />
+						<Route exact path="/home" component={Home} />
+					</Switch>
+				</Router>
+			</Provider>
 		);
 	}
 }
