0680f70f60f773101ceaf77fdcb21e300b0d3483
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-16 23:50:37 +0700

    :inbox_tray: [mobx][store][init]: Created AuthStore & its actions and observable

--- client/src/mobx/stores/RootStore.js
+++ client/src/mobx/stores/RootStore.js
@@ -1,6 +1,7 @@
 import { createContext } from 'react';
 import { GlobalStore } from './GlobalStore/GlobalStore';
-
+import { AuthStore } from './AuthStore/AuthStore';
 export class RootStore {
 	globalStore = new GlobalStore(this);
+	AuthStore = new AuthStore(this);
 }
