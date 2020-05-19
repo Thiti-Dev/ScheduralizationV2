6650178f59b4b35c417b9ab9dee3ebbcc50b3753
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-19 15:09:11 +0700

    :gem: [schedule-utils]: Created convertClockTimeToMinutesFromStart

---
+++ client/src/components/pages/Schedule/helper.js
@@ -0,0 +1,32 @@
+//
+// ─── UTIL ───────────────────────────────────────────────────────────────────────
+//
+import { isHavingDecimalPlaceThatGreaterThanZero } from '../../../utils/mathHelper';
+// ────────────────────────────────────────────────────────────────────────────────
+
+//
+// ─── CONST ──────────────────────────────────────────────────────────────────────
+//
+const start_from = '8'; // 8.00 AM => the earliest time that the courses can be started
+// ────────────────────────────────────────────────────────────────────────────────
+
+export function convertClockTimeToMinutesFromStart(raw_time, _start_from = start_from) {
+	const _based_hour = parseInt(raw_time.toString().split('.')[0]);
+
+	let additional_from_custom_start_from = 0;
+	let based_hour_start_from = 0;
+	if (isHavingDecimalPlaceThatGreaterThanZero(parseFloat(_start_from))) {
+		additional_from_custom_start_from = parseInt(_start_from.toString().split('.')[1]);
+		based_hour_start_from = parseInt(_start_from.toString().split('.')[0]);
+	} else {
+		based_hour_start_from = parseInt(_start_from);
+	}
+	const _raw_time = _based_hour - based_hour_start_from;
+	let addition_minute_value_needed = 0;
+	if (isHavingDecimalPlaceThatGreaterThanZero(parseFloat(raw_time))) {
+		addition_minute_value_needed = parseInt(raw_time.toString().split('.')[1]);
+	}
+	// if found decmimal places
+
+	return Math.abs(_raw_time * 60 + addition_minute_value_needed - additional_from_custom_start_from); // minutes form
+}
