b421c1938d349cf24557156a99938a02ef1ea6a9
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-20 22:26:16 +0700

    :card_file_box: [sequelize][model]: Created CourseAvailables & its migration

---
+++ migrations/20200519160550-create-course-available.js
@@ -0,0 +1,71 @@
+'use strict';
+module.exports = {
+	up: (queryInterface, Sequelize) => {
+		return queryInterface.createTable('CourseAvailables', {
+			id: {
+				allowNull: false,
+				autoIncrement: true,
+				primaryKey: true,
+				type: Sequelize.INTEGER
+			},
+			courseID: {
+				type: Sequelize.STRING,
+				allowNull: false,
+				references: {
+					model: 'Courses',
+					key: 'courseID',
+					as: 'courseID'
+				}
+			},
+			semester: {
+				type: Sequelize.INTEGER,
+				allowNull: false
+			},
+			credit: {
+				type: Sequelize.INTEGER,
+				allowNull: false
+			},
+			totalSeat: {
+				type: Sequelize.INTEGER,
+				allowNull: false
+			},
+			section: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			allowedGroup: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			day: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			start: {
+				type: Sequelize.STRING,
+				allowNull: true
+			},
+			end: {
+				type: Sequelize.STRING,
+				allowNull: true
+			},
+			classroom: {
+				type: Sequelize.STRING,
+				allowNull: false
+			},
+			createdAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('now')
+			},
+			updatedAt: {
+				allowNull: false,
+				type: Sequelize.DATE,
+				defaultValue: Sequelize.fn('now')
+			}
+		});
+	},
+	down: (queryInterface, Sequelize) => {
+		return queryInterface.dropTable('CourseAvailables');
+	}
+};
