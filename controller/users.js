784aacd01a7e1e7b8ce4a4247d554b7548f970c3
Thiti Dev <thiti.mwk.main@gmail.com>
2020-05-22 22:00:29 +0700

    :zap: [users-controller]: Created (API)updateStudiedCourses

--- controller/users.js
+++ controller/users.js
@@ -24,3 +24,28 @@ exports.getSpecificData = asyncHandler(async (req, res, next) => {
 
 	res.status(200).json({ success: true, data: user_data });
 });
+
+// @desc    Get specific data of the current user
+// @route   PUT /api/users/updateStudiedCourses
+// @acess   Private
+exports.updateStudiedCourses = asyncHandler(async (req, res, next) => {
+	const { courses_plain_str } = req.body;
+	if (!courses_plain_str && courses_plain_str !== '') {
+		return next(new ErrorResponse(`Missing request body (courses_plain_str)`, 400));
+	}
+	const user_data = await User.findByPk(req.user.id);
+	if (!user_data) {
+		return next(new ErrorResponse(`Your account isn't found on the database`, 400));
+	}
+	//
+	// ─── Data(Mess) cleanner
+	//
+	const finalized_courses_plain_str = courses_plain_str.split(',').filter(Boolean).join(',');
+	// ────────────────────────────────────────────────────────────────────────────────
+
+	user_data.learnedCourses = finalized_courses_plain_str;
+
+	await user_data.save();
+
+	res.status(200).json({ success: true, data: finalized_courses_plain_str });
+});
--- routes/api/users.js
+++ routes/api/users.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router();
 // Importing function from the controller
-const { getSpecificData } = require('../../controller/users.js');
+const { getSpecificData, updateStudiedCourses } = require('../../controller/users.js');
 
 const { protect } = require('../../middleware/auth');
 
 router.route('/getSpecificData').get(protect, getSpecificData);
+router.route('/updateStudiedCourses').put(protect, updateStudiedCourses);
 
 module.exports = router;
